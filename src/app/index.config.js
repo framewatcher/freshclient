4f46eca63817bde521a361a79d8a71ba912109b8
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2021-12-31 10:36:39 +0800

    updated from windows

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,25 +1,34 @@
 (function() {
   'use strict';
   angular
-    .module('formioApp')
-    .config([
-      'AppConfig',
-      'FormioProvider',
-      'FormioAuthProvider',
-      '$locationProvider',
-      function(
-        AppConfig,
-        FormioProvider,
-        FormioAuthProvider,
-        $locationProvider
-      ) {
-        $locationProvider.hashPrefix('');
-        FormioProvider.setAppUrl(AppConfig.appUrl);
-        FormioProvider.setApiUrl(AppConfig.apiUrl);
-        FormioAuthProvider.setForceAuth(true);
-        FormioAuthProvider.setStates('auth.login', 'home');
-        FormioAuthProvider.register('login', 'user', 'login');
-
-      }
-    ]);
+  .module('formioApp')
+  .config([
+    'AppConfig',
+    'FormioProvider',
+    'FormioAuthProvider',
+    '$locationProvider',
+    function(
+      AppConfig,
+      FormioProvider,
+      FormioAuthProvider,
+      $locationProvider
+    ) {
+      $locationProvider.hashPrefix('');
+      FormioProvider.setAppUrl(AppConfig.appUrl);
+      FormioProvider.setApiUrl(AppConfig.apiUrl);
+      FormioAuthProvider.setForceAuth(true);
+      FormioAuthProvider.setStates('auth.login', 'home');
+      FormioAuthProvider.register('login', 'user', 'login');
+      FormioResourceProvider.register('customer', AppConfig.vendor, {
+        templates: {
+          view: 'views/user/vendor.html'
+        },
+        controllers: {
+          view: ['$scope', '$rootScope', function($scope, $rootScope) {
+             $scope.myuid = $rootScope.user._id;
+          }]
+        }
+      });
+    }
+  ]);
 })();
