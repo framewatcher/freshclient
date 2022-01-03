644da686668483ea36c5dfaebcac7f9fd794cfcb
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2022-01-03 11:42:27 +0800

    updated from windows

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -83,7 +83,7 @@
       .state('vendorlist', {
         url: '/vendorlist',
         templateUrl: 'views/user/vendor.html',
-        controller: ['$scope', '$rootScope', '$http', '$Formio', function($scope, $rootScope, $http, Formio) {
+        controller: ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
           $scope.myuid = $rootScope.user._id;
           // $http.get('http://localhost:3001/#/viewvendorlist', {headers: {'x-jwt-token': Formio.getToken()} }).then(function(result){ $scope.getvendorlist = JSON.stringify(result.data); alert(result); });
         }]
