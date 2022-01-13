99900df039e4067ac1f0cbb073f6debb44a48f11
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2022-01-13 15:55:21 +0800

    updated from windows

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -125,6 +125,11 @@
           $scope.myuid = $rootScope.user._id;
           $http.get('http://localhost:3001/irf/submission', {headers: {'x-jwt-token': Formio.getToken()} }).then(function(result){ $scope.getirflist = result.data; });
           $http.get('http://localhost:3001/quotationItem/submission', {headers: {'x-jwt-token': Formio.getToken()} }).then(function(result){ $scope.quotationItemlist = result.data; });
+          $scope.gmyarray = [];
+          angular.forEach($scope.getirflist,function(value,index){
+                $scope.gmyarray.push(value) ;
+            })
+          
         }]
       });
 
