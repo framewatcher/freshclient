ed37ec715d76a060cb483c3a6fb610feb0b6e2e3
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2022-01-13 15:17:52 +0800

    updated from windows

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -126,6 +126,7 @@
           $http.get('http://localhost:3001/irf/submission', {headers: {'x-jwt-token': Formio.getToken()} }).then(function(result){ $scope.getirflist = result.data; });
           $http.get('http://localhost:3001/quotationItem/submission', {headers: {'x-jwt-token': Formio.getToken()} }).then(function(result){ $scope.quotationItemlist = result.data; });
           var mx = [];
+          $scope.mtx = [];
           var x = 0;
           while (x<2){
             mx = $scope.getirflist[x]._id;
