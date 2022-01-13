d858cfe29a4870d7a83e4506bbdcd2ad25cbf543
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2022-01-13 09:23:32 +0800

    updated from windows

--- dist/config.js
+++ dist/config.js
@@ -16,6 +16,7 @@ angular.module('formioApp').constant('AppConfig', {
   forms: {
     vendorForm: appUrl + '/vendor',
     irfForm: appUrl + '/irf',
+    qtitem: appUrl + '/quotationitem',
     vendorall: appUrl + '/#/viewvendorlist',
     userForm: appUrl + '/user',
     userLoginForm: appUrl + '/user/login'
