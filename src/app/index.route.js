fb169c834b09b11ebd366730c84fb4262b5076d3
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2022-01-19 16:01:41 +0800

    updated from windows

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -138,22 +138,29 @@
                 joinirfitem[y][newUser] = newValue ;
               }
             });
-
             $http.get('http://localhost:3001/quotationItem/submission', {headers: {'x-jwt-token': Formio.getToken()} }).then(
               function(result){
                 $scope.quotationItemlist = result.data;
                 var irfitemdata = result.data;
 
               });
+              $http.get('http://localhost:3001/qtitemreply/submission', {headers: {'x-jwt-token': Formio.getToken()} }).then(
+                function(result){
+                  $scope.quotationitemreplylist = result.data;
+                });
 
-            $http.get('http://localhost:3001/qtitemreply/submission', {headers: {'x-jwt-token': Formio.getToken()} }).then(
-              function(result){
-                $scope.quotationitemreplylist = result.data;
+                $scope.gmyarray = joinirfitem;
+
+                $scope.showirfitem = function(irfid) {
+                  $http.get('http://localhost:3001/quotationItem/submission?data.ftirf='+irfid, {headers: {'x-jwt-token': Formio.getToken()} }).then(
+                    function(result){
+                      $scope.showirfitemlist = result.data;
+                    });
+                  };
+
+
+                }]
               });
-
-            $scope.gmyarray = joinirfitem;
-        }]
-      });
 
       $stateProvider
       .state('qtitemadd', {
